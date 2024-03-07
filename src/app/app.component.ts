41ebd7157c285ed7f08415232e5632da243e4b79
Jesus Cordoba Juarez <jesus.cordoba@oreillyauto.mx>
2024-03-07 17:21:17 -0600

    Color palette

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular';
+  colores: string[] = [
+    'background-green50',
+    'background-green100',
+    'background-green200',
+    'background-green300',
+    'background-green400',
+    'background-green500',
+    'background-green600',
+    'background-green700',
+    'background-green800',
+    'background-green900'
+  ];
 }
